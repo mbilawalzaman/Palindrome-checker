e2a26d9dc65ed7d2f667d53e538a18837c375808
Muhammad Bilawal Zaman <143614305+mbilawalzaman@users.noreply.github.com>
2024-10-18 19:43:15 +0000

    Main Fix palindrome function to handle case sensitivity

--- src/components/Palindrome.jsx
+++ src/components/Palindrome.jsx
@@ -1,4 +1,5 @@
 export const isPalindrome = (string) => {
+  string = string.toLowerCase();
   let length = string.length;
   for (let i = 0; i < length / 2; i++) {
     if (string[i] !== string[length - 1 - i]) {

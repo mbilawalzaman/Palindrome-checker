9472d26d2b807133c66c2f315a18f83f04eacb8d
Muhammad Bilawal Zaman <143614305+mbilawalzaman@users.noreply.github.com>
2024-10-18 20:37:24 +0000

    Main: Added clear function to reset screen

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,11 @@ function App() {
     setResult(isPalindrome(input));
   };
 
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+
   return (
     <div className="flex items-center justify-center bg-gray-100 h-screen w-screen">
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
@@ -31,14 +36,28 @@ function App() {
               placeholder="Enter text"
               className="mt-4 border border-gray-300 rounded-md p-2 w-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            <button
-              type="button"
-              onClick={handleCheck}
-              className="w-full bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600 transition"
-            >
-              Check
-            </button>
+            <div className="dispaly flex space-x-4">
+              <button
+                type="button"
+                onClick={handleCheck}
+                className="w-full bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600 transition"
+              >
+                Check
+              </button>
+              <button
+                type="button"
+                onClick={handleClear}
+                className="w-full bg-red-500 text-white rounded-md p-2 hover:bg-red-600 transition"
+              >
+                Clear
+              </button>
+            </div>
           </form>
+        </div>
+        <div className="mt-5 underline font-bold">NOTE:</div>
+        <div className="mt-1">
+          A palindrome is a word or sentence that's spelled the same way both
+          forward and backward, ignoring punctuation, case, and spacing.
         </div>
       </div>
     </div>
